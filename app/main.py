2034bba36cbdbc7c91f9f751f1fabc27e8f75a95
Neil Kleynhans <ntkleynhans@gmail.com>
2023-03-01 13:49:52 +0200

    adding all cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from typing import Union
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
